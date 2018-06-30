177b3b85ea598129cfeed5a81815cf1389333ea6
Gabriel Perez Salgado <gabopersa@gmail.com>
2018-06-29 21:10:07 -0400

    Esqueleto del script

---
+++ Proyecto.R
@@ -0,0 +1,14 @@
+# Lab4 - Estadistica
+# Nombres : Maria Victoria Oliveros 14-10755
+#           Gabriel Perez Salgado   13-11104
+#           Paul Baptista           10-10056
+
+# Lectura base de datos
+ datos<-proceso
+ attach(datos)
+ summary(datos)
+
+ 
+ 
+
+ 
