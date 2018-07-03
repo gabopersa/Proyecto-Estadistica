a950783e4dd4369dea9ccda5fe99f8f04e16ab0d
Gabriel Perez Salgado <gabopersa@gmail.com>
2018-07-02 21:53:06 -0400

    Prediccion de la variable y

--- Proyecto.R
+++ Proyecto.R
@@ -25,7 +25,8 @@ library(knitr)  # Para poder sacar una tabla bonita en html con kable
 
 # Valores calculados para las distintas variables encontradas en las muestras independientes del Centro de Investigacion 
   kable(analisis(datos))
-
+  plot(Cent, main = "Centros de Investigacion", col = c("red","yellow","orange","brown"), ylab = "Frecuencia", ylim = c(0,70))
+  
 # Valores calculados para las distintas variables encontradas en las muestras dependiendo del Centro de Investigacion 
   datosCentros<-split(datos, Cent)
   kable(analisis(datosCentros$A))
@@ -34,10 +35,12 @@ library(knitr)  # Para poder sacar una tabla bonita en html con kable
   kable(analisis(datosCentros$D))
   
 # Histogramas para cada variable independiente del Centro de Investigacion
+  par(mfrow =c(2,2))
   hist  (y, main = "Porcentaje de óxido de aluminio que precipita.", ylab = "Frecuencia", xlab = "Porcentaje")
   hist  (x1, main = "Concentración del ácido.", ylab = "Frecuencia", xlab = "Contracion", ylim = c(0,25))
   hist  (x2, main = "Valor final de pH de la solución en unidades de pH.", ylab = "Frecuencia", xlab = "pH")
   hist  (x3, main = "Temperatura del proceso en grados centígrados.", ylab = "Frecuencia", xlab = "Grados Centigrados", ylim = c(0,70))
+  par(mfrow =c(2,2))
   hist  (x4, main = "Concentración de la base.", ylab = "Frecuencia", xlab = "Concentracion")
   hist  (x5, main = "Velocidad de agitación en rpm.", ylab = "Frecuencia", xlab = "RPM", xlim = c(0,2000))
   hist  (x6, main = "Velocidad de adición de la base en mililitros por hora.", ylab = "Frecuencia", xlab = "Mililitros por hora", ylim = c(0,60))
@@ -101,17 +104,19 @@ library(knitr)  # Para poder sacar una tabla bonita en html con kable
   }
 
 # Diagramas de Caja para cada variable independientes del Centro de Ivestigacion
-  par(mfrow = c(1,1))
+  par(mfrow = c(2,2))
   boxplot(y, main = "Porcentaje de óxido de aluminio que precipita.")
   boxplot(x1, main = "Concentración del ácido.")
   boxplot(x2, main = "Valor final de pH de la solución en unidades de pH.")
   boxplot(x3, main = "Temperatura del proceso en grados centígrados.")
+  par(mfrow = c(2,2))
   boxplot(x4, main = "Concentración de la base.")
   boxplot(x5, main = "Velocidad de agitación en RPM.")
   boxplot(x6, main = "Velocidad de adición de la base en mililitros por hora.")
   boxplot(x7, main = "Tiempo del proceso en horas.")
 
 # Diagramas de caja para cada variable dependiendo del Centro de Investigacion
+  par(mfrow = c(1,1))
   boxplot(Y$A, Y$B, Y$C, Y$D, main = "Porcentaje de óxido de aluminio que precipita.", names = c("A","B","C","D") )  
   boxplot(X1$A, X1$B, X1$C, X1$D, main = "Concentración del ácido.", names = c("A","B","C","D") )  
   boxplot(X2$A, X2$B, X2$C, X2$D, main = "Valor final de pH de la solución en unidades de pH.", names = c("A","B","C","D") )  
@@ -126,24 +131,10 @@ library(knitr)  # Para poder sacar una tabla bonita en html con kable
   datosSinCentro<-subset(datos, select = y:x7)
   pairs(datosSinCentro)
   
-# Grafico de dispersion de las variables dependiendo del Centro de investigacion
-  A<-subset(datosCentros$A, select = y:x7)
-  B<-subset(datosCentros$B, select = y:x7)
-  C<-subset(datosCentros$C, select = y:x7)
-  D<-subset(datosCentros$D, select = y:x7)
-  pairs(A)
-  pairs(B)
-  pairs(C)
-  pairs(D)
 
 # Matriz de Correlacion de las variables independientes del Centro de Investigacion
   cor(datosSinCentro)
 
-# Matriz de Correlacion de las variables dependiento del Centro de Investigacion
-  cor(A)
-  cor(B)
-  cor(C)
-  cor(D)
 
 # Prueba de bondad de ajuste para determinar si la variable “y” se distribuye en forma normal.
   
@@ -184,46 +175,6 @@ library(knitr)  # Para poder sacar una tabla bonita en html con kable
   t.test(datos[7], conf.level = 0.93)  
   t.test(datos[8], conf.level = 0.93)
   
-  # Centro de Investigacion A
-  t.test(A[1], conf.level = 0.93)  
-  t.test(A[2], conf.level = 0.93)  
-  t.test(A[3], conf.level = 0.93)  
-  t.test(A[4], conf.level = 0.93)  
-  t.test(A[5], conf.level = 0.93)  
-  t.test(A[6], conf.level = 0.93)  
-  t.test(A[7], conf.level = 0.93)  
-  t.test(A[8], conf.level = 0.93)
-  
-  # Centro de Investigacion B
-  t.test(B[1], conf.level = 0.93)  
-  t.test(B[2], conf.level = 0.93)  
-  t.test(B[3], conf.level = 0.93)  
-  t.test(B[4], conf.level = 0.93)  
-  t.test(B[5], conf.level = 0.93)  
-  t.test(B[6], conf.level = 0.93)  
-  t.test(B[7], conf.level = 0.93)  
-  t.test(B[8], conf.level = 0.93)
-  
-  # Centro de Investigacion C
-  t.test(C[1], conf.level = 0.93)  
-  t.test(C[2], conf.level = 0.93)  
-  t.test(C[3], conf.level = 0.93)  
-  t.test(C[4], conf.level = 0.93)  
-  t.test(C[5], conf.level = 0.93)  
-  t.test(C[6], conf.level = 0.93)  
-  t.test(C[7], conf.level = 0.93)  
-  t.test(C[8], conf.level = 0.93)
-  
-  # Centro de Investigacion D
-  t.test(D[1], conf.level = 0.93)  
-  t.test(D[2], conf.level = 0.93)  
-  t.test(D[3], conf.level = 0.93)  
-  t.test(D[4], conf.level = 0.93)  
-  t.test(D[5], conf.level = 0.93)  
-  t.test(D[6], conf.level = 0.93)  
-  t.test(D[7], conf.level = 0.93)  
-  t.test(D[8], conf.level = 0.93)
-
 
 # Halle un modelo lineal que explique mejor la variable “y”.  
   
@@ -256,113 +207,29 @@ library(knitr)  # Para poder sacar una tabla bonita en html con kable
   m7 = lm( y ~ x6 +x7 - 1)
   summary(m7)
   
-# Modelo para el Centro de investigacion A
-  # Primer modelo con todas las variables
-  mA1 = lm(A$y ~ A$x1 + A$x2 + A$x3 + A$x4 + A$x5 + A$x6 + A$x7)
-  summary(mA1)
+  # Analisis de Residuos
+  par(mfrow = c(2,2))
+  plot(m7)
   
-  # Segundo modelo excluyendo A$x5
-  mA2 = lm(A$y ~ A$x1 + A$x2 + A$x3 + A$x4 + A$x6 + A$x7)
-  summary(mA2)
+  # Estudio de independencia
+  for(i in 1:8)
+  {
+    par(mfrow = c(1,1))
+    plot(datos[[i]], rstandard(m2), xlab = names(datos[i]), main="Estudio de Independencia")
+  }
+  
+# Prediccion de la variable "y"
+  datos2<-proceso_pre
+  attach(datos2)
+  y_mod = 0.0183364*x6 + 3.1004344*x7
+  error = y - y_mod 
+  error
+  length(error)
+  summary(error)
+  sd(error)
+  hist(error, main = "Errores", ylab = "Frecuencia", xlab = "Error")
+  boxplot(error, main = "Error")
+  
 
-  # Tercer modelo excluyendo A$x5 y A$x3
-  mA3 = lm(A$y ~ A$x1 + A$x2 + A$x4 + A$x6 + A$x7)
-  summary(mA3)
-  
-  # Cuarto modelo excluyendo A$x5, A$x3 y el interceptor
-  mA4 = lm(A$y ~ A$x1 + A$x2 + A$x4 + A$x6 + A$x7 - 1)
-  summary(mA4)
-  
-  # Quinto modelo excluyendo A$x5, A$x3, el interceptor y x1
-  mA5 = lm(A$y ~ A$x2 + A$x4 + A$x6 + A$x7 - 1)
-  summary(mA5)
 
-  # Sexto modelo excluyendo A$x5, A$x3, el interceptor, A$x1, A$x2
-  mA6 = lm(A$y ~ A$x4 + A$x6 + A$x7 - 1)
-  summary(mA6)
-  
-# Modelo para el Centro de investigacion B
-  # Primer modelo con todas las variables
-  mB1 = lm(B$y ~ B$x1 + B$x2 + B$x3 + B$x4 + B$x5 + B$x6 + B$x7)
-  summary(mB1)
-
-  # Segundo excluyendo a B$x3
-  mB2 = lm(B$y ~ B$x1 + B$x2 + B$x4 + B$x5 + B$x6 + B$x7)
-  summary(mB2)
-  
-  # Tercer modelo excluyendo a B$x3 y a B$x2
-  mB3 = lm(B$y ~ B$x1 + B$x4 + B$x5 + B$x6 + B$x7)
-  summary(mB3)
-
-  # Cuarto modelo excluyendo a B$x3, B$x2 y a B$x5
-  mB4 = lm(B$y ~ B$x1 + B$x4 + B$x6 + B$x7)
-  summary(mB4)
-  
-  # Quinto modelo excluyendo a B$x3, B$x2, B$x5 y a B$x4
-  mB5 = lm(B$y ~ B$x1+ B$x6 + B$x7)
-  summary(mB5)
-
-  # Sexto modelo excluyendo a B$x3, B$x2, B$x5, B$x4 y al interceptor
-  mB6 = lm(B$y ~ B$x1+ B$x6 + B$x7 - 1)
-  summary(mB6)
-
-  # Sexto modelo excluyendo a B$x3, B$x2, B$x5, B$x4, al interceptor y a B$x7
-  mB7 = lm(B$y ~ B$x1+ B$x6 - 1)
-  summary(mB7)
-
-# Modelo para el Centro de investigacion C
-  # Primer modelo con todas las variables
-  mC1 = lm(C$y ~ C$x1 + C$x2 + C$x3 + C$x4 + C$x5 + C$x6 + C$x7)
-  summary(mC1)
-  
-  # Segundo modelo excluyendo a C$x2
-  mC2 = lm(C$y ~ C$x1 + C$x3 + C$x4 + C$x5 + C$x6 + C$x7)
-  summary(mC2)
-  
-  # Tercer modelo excluyendo a C$x2 y a C$x3
-  mC3 = lm(C$y ~ C$x1 + C$x4 + C$x5 + C$x6 + C$x7)
-  summary(mC3)
-
-  # Cuarto modelo excluyendo a C$x2, C$x3 y al interceptor
-  mC4 = lm(C$y ~ C$x1 + C$x4 + C$x5 + C$x6 + C$x7 - 1)
-  summary(mC4)
-
-  # Quinto modelo excluyendo a C$x2, C$x3, al interceptor y a C$x1
-  mC5 = lm(C$y ~ C$x4 + C$x5 + C$x6 + C$x7 - 1)
-  summary(mC5)
-  
-  # Sexto modelo excluyendo a C$x2, C$x3, al interceptor, C$x1 y a C$x5
-  mC6 = lm(C$y ~ C$x4 + C$x6 + C$x7 - 1)
-  summary(mC6)
-  
-  # Septimo modelo excluyendo a C$x2, C$x3, al interceptor, C$x1, C$x5 y a C$x4
-  mC7 = lm(C$y ~ C$x6 + C$x7 - 1)
-  summary(mC7)
-  
-# Modelo para el Centro de investigacion D
-  # Primer modelo con todas las variables
-  mD1 = lm(D$y ~ D$x1 + D$x2 + D$x3 + D$x4 + D$x5 + D$x6 + D$x7)
-  summary(mD1)
-  
-  # Segundo modelo excluyendo a D$x4
-  mD2 = lm(D$y ~ D$x1 + D$x2 + D$x3 + D$x5 + D$x6 + D$x7)
-  summary(mD2)
-  
-  # Tercer modelo excluyendo a D$x4 y al interceptor
-  mD3 = lm(D$y ~ D$x1 + D$x2 + D$x3 + D$x5 + D$x6 + D$x7 - 1)
-  summary(mD3)
-  
-  # Cuarto modelo excluyendo a D$x4, al interceptor y a D$x3 
-  mD4 = lm(D$y ~ D$x1 + D$x2 + D$x5 + D$x6 + D$x7 - 1)
-  summary(mD4)
-  
-  # Quinto modelo excluyendo a D$x4, al interceptor, D$x3 y a D$x1
-  mD5 = lm(D$y ~ D$x2 + D$x5 + D$x6 + D$x7 - 1)
-  summary(mD5)
-
-  # Quinto modelo excluyendo a D$x4, al interceptor, D$x3, D$x1 y a D$x5
-  mD6 = lm(D$y ~ D$x2 + D$x6 + D$x7 - 1)
-  summary(mD6)
-  
-  
 #######################   FIN   #############################
